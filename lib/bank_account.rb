493270746866d4cbe208be76d249c9b4c4f2102c
adudley78 <dudleyadam@gmail.com>
2018-12-09 14:17:15 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,43 @@
+require 'pry'
+
 class BankAccount
+  attr_reader :name
+  attr_accessor :acct, :balance, :status
+
+  # @name = [ ]
+
+  def initialize(name)
+    @acct = acct
+    @name = name
+    @balance = 1000
+    @status = 'open'
+    # @closed = false
+    # @valid = true
+  end
+
+  def deposit(money)
+    @balance = @balance + money
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    # binding.pry
+    @balance > 0 && @status == 'open' #here I'm checking that the receiver account is valid
+    # sender = Transfer.new
+    #check that sender account is valid
+      #need to check that sender acct has balance < 0 and status is open
+      # Transfer.sender.balance < 0 && Transfer.sender.status == 'open'
+  end
+
+  def close_account
+    @status = 'closed'
+  end
+
+  # def add_name
+  #   @name << name.freeze
+  # end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,48 @@
+require 'pry'
+
 class Transfer
-  # your code here
+  attr_accessor :status
+  attr_reader :amount, :sender, :receiver
+
+  def initialize(sender, receiver, amount)
+    # @transfer = transfer
+    @sender = sender
+    @receiver = receiver
+    @status = 'pending'
+    @amount = amount
+  end
+
+# binding.pry
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    #binding.pry
+    # c = 0
+    # while c < 1 do
+    if self.status == "pending" && self.sender.valid? && self.sender.balance > amount
+      self.sender.balance = self.sender.balance - amount
+      self.receiver.balance = self.receiver.balance + amount
+      self.status = 'complete'
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+      # if !@status =
+      # c += 1
+    # end
+    #subtract amount from senders balance
+      #add to receivers balance
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      self.sender.balance = self.sender.balance + amount
+      self.receiver.balance = self.receiver.balance - amount
+      self.status = 'reversed'
+    end
+  end
+
 end
